b3327d0adc022d0880b80950b5a0729bffe33375
Christian Schmidt <cjschmidt33@gmail.com>
2024-09-08 19:31:00 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -9,3 +9,21 @@ function calculateAverageSales(sales) {
 }
 
 console.log(calculateAverageSales(testSales)); // output: 2.5 (10/4)
+
+
+// Task 2: Create a Function to Determine Performance Rating
+
+let testPerformanceRating = 7000
+
+function determinePerformanceRating(averageSales) {
+    if (averageSales > 10000)
+        return "Excellent"
+    if (7000 < averageSales <= 10000)
+        return "Good"
+    if (4000 < averageSales <= 7000)
+        return "Satisfactory"
+    if (averageSales <= 4000)
+        return "Needs Improvement"
+}
+
+console.log(determinePerformanceRating(testPerformanceRating)); //output: Good
